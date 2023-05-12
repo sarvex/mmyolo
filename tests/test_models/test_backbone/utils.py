4cc1e6344cbaa38795521ae43394d4cc65d4ac04
Sourcery AI <>
2023-05-12 06:25:07 +0000

    'Refactored by Sourcery'

--- tests/test_models/test_backbone/utils.py
+++ tests/test_models/test_backbone/utils.py
@@ -9,23 +9,26 @@ from torch.nn.modules.batchnorm import _BatchNorm
 
 def is_block(modules):
     """Check if is ResNet building block."""
-    if isinstance(modules, (BasicBlock, Bottleneck, BottleneckX, Bottle2neck,
-                            SimplifiedBasicBlock)):
-        return True
-    return False
+    return isinstance(
+        modules,
+        (
+            BasicBlock,
+            Bottleneck,
+            BottleneckX,
+            Bottle2neck,
+            SimplifiedBasicBlock,
+        ),
+    )
 
 
 def is_norm(modules):
     """Check if is one of the norms."""
-    if isinstance(modules, (GroupNorm, _BatchNorm)):
-        return True
-    return False
+    return isinstance(modules, (GroupNorm, _BatchNorm))
 
 
 def check_norm_state(modules, train_state):
     """Check if norm layer is in correct train state."""
-    for mod in modules:
-        if isinstance(mod, _BatchNorm):
-            if mod.training != train_state:
-                return False
-    return True
+    return not any(
+        isinstance(mod, _BatchNorm) and mod.training != train_state
+        for mod in modules
+    )
